564561f02aec3ba671657af6b79e2a2fe407384e
vittosba <vitsbardella@gmail.com>
2022-01-23 17:33:48 +0100

    movies model controller and route

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+class MovieController extends Controller
+{
+    /**
+     * MOVIE ARCHIVE
+     */
+
+     public function index() {
+         $movies = Movie::all();
+         dump($movies);
+     }
+}
